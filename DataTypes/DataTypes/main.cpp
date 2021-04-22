d41b12de44491c569005e20a28ca6b45b1c3e5bc
Clayman <Clayman@DESKTOP-649SK3N>
2021-04-22 19:59:39 +0300

    Declaration of Variables

--- DataTypes/DataTypes/main.cpp
+++ DataTypes/DataTypes/main.cpp
@@ -1,22 +1,64 @@
-#include<iostream>
+﻿#include<iostream>
 using namespace std;
+using std::cin;
+using std::cout;
+using std::endl;;
+
+//#define TYPES
+#define DECLARATION_OF_VARIABLES
 
 void main()
 {
 	setlocale(LC_ALL, "Russian");
+#ifdef TYPES
 	//cout << false << endl;
 
-	/*cout << sizeof(short) << endl;
-	cout << "unsigned short:\t" << 0 << "\t" << USHRT_MAX << endl;
-	cout << "  signed short:\t" << SHRT_MIN << "\t" << SHRT_MAX << endl;
+/*cout << sizeof(short) << endl;
+cout << "unsigned short:\t" << 0 << "\t" << USHRT_MAX << endl;
+cout << "  signed short:\t" << SHRT_MIN << "\t" << SHRT_MAX << endl;
 
-	cout << sizeof(long) << endl;
-	cout << "unsigned long:\t\t" << 0 << "\t" << ULONG_MAX << endl;
-	cout << "  signed long:" << LONG_MIN << "\t" << LONG_MAX << endl;*/
+cout << sizeof(long) << endl;
+cout << "unsigned long:\t\t" << 0 << "\t" << ULONG_MAX << endl;
+cout << "  signed long:" << LONG_MIN << "\t" << LONG_MAX << endl;*/
 
-	//float  - 4 Bytes
-	//double - 8 Bytes
+//float  - 4 Bytes
+//double - 8 Bytes
 	cout << sizeof(double) << endl;
 	cout << DBL_MIN << "\t" << DBL_MAX << endl;
 	cout << -3.5e-3 << endl;
+#endif // TYPES
+
+#ifdef DECLARATION_OF_VARIABLES
+	double Price;	//Объявление переменной Price, типа double
+	//cout << Price << endl;
+	//int st1;//1-й
+	//int _void;
+
+	double a;
+	int b;
+	int t;
+
+	//Инициализация переменных
+	//ПРОИНИЦИАЛИЗИРОВАТЬ
+	//Инициализация - это присвоение начального занчения. (Init - Начало)
+
+	//			CamelCase/PascalCase
+	//double PriceOfCoffe;	//Camel-case style
+	//						//BigCamel
+	//double priceOfCoffe;	//smallCamel
+
+	//Snake-case style
+	double price_of_coffee;	//Объявление переменной
+	int number_of_cups = 0;	//Объявление и инициализация переменной
+	
+	cout << "Введите стоимость одной чашки кофе: ";
+	cin >> price_of_coffee;	//ИНИЦИАЛИЗАЦИЯ вводом с клавиатуры
+	cout << "Сколько чашек Вы хотите купить? ";
+	cin >> number_of_cups;
+
+	double total_price = price_of_coffee * number_of_cups;
+	cout << "Общая стоимость кофе: " << total_price << endl;
+
+#endif // DECLARATION_OF_VARIABLES
+
 }
