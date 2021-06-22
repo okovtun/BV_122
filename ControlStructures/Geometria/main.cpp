f3b66bfa994adaf5bf95564dcfabcd074638e445
Clayman <Clayman@DESKTOP-DM8E77U>
2021-06-22 23:22:11 +0300

    Geometria day 1

--- ControlStructures/Geometria/main.cpp
+++ ControlStructures/Geometria/main.cpp
@@ -6,7 +6,18 @@ using namespace std;
 //#define TRIANGLE_2
 //#define TRIANGLE_3
 #define TRIANGLE_4
-#define RUMBUS
+//#define ROMBUS
+//#define PLUS_MINUS
+#define ASCII
+
+#define UPPER_LEFT_ANGLE	(char)218
+#define UPPER_RIGHT_ANGLE	(char)191
+#define DOWN_LEFT_ANGLE		(char)192
+#define DOWN_RIGHT_ANGLE	(char)217
+#define VERT_LINE			(char)179
+#define HORIZ_LINE			(char)196 << (char)196
+#define WHITE_BOX			"\xDB\xDB"//(char)219 << (char)219
+#define BLACK_BOX			"\x20\x20"//(char)32  << (char)32
 
 void main()
 {
@@ -15,7 +26,7 @@ void main()
 	cout << "Введите размер фигуры: "; cin >> n;
 
 #ifdef SQUARE
-	for (int i = 0; i < n; i++)//Этот for повторяет вывод строки
+	for (int i = 0; i < n; i++)//Этот for повторяет вывод строки, следовательно, его счетчик 'i' считает строки
 	{
 		for (int j = 0; j < n; j++)	//Этот for выводит строку из звездочек, 
 									//повторяя вывод звездочки на экран.
@@ -59,12 +70,13 @@ void main()
 	}
 #endif // TRIANGLE_3
 
+#ifdef ROMBUS
 	for (int i = 0; i < n; i++)
 	{
 		for (int j = i; j < n; j++)
 			cout << " ";
 		cout << "/";
-		for (int j = 0; j < i*2; j++)
+		for (int j = 0; j < i * 2; j++)
 			cout << ".";
 		cout << "\\";
 		cout << endl;
@@ -72,7 +84,48 @@ void main()
 	for (int i = 0; i < n; i++)
 	{
 		for (int j = 0; j <= i; j++)cout << " ";	cout << "\\";
-		for (int j = i; j < n - 1; j++)cout << " ";cout << "/";
+		int k = n - i;
+		for (int j = 0; j < k * 2; j++)cout << " "; cout << "/";
+		cout << endl;
+	}
+#endif // ROMB
+
+#ifdef PLUS_MINUS
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			//if ((j+i) % 2 == 0)cout << "+ ";else cout << "- ";
+			//(i + j) % 2 == 0 ? cout << "+ " : cout << "- ";
+			cout << ((i + j) % 2 == 0 ? "+ " : "- ");
+		}
+		cout << endl;
+	}
+#endif // PLUS_MINUS
+
+	setlocale(LC_ALL, "C");	//Возвращает кодировку по умолчанию
+	/*for (int i = 0; i < 256; i++)
+	{
+		cout << i << "\t" << (char)i << endl;
+	}*/
+	n++;
+	for (int i = 0; i <= n; i++)
+	{
+		for (int j = 0; j <= n; j++)
+		{
+			if (i == 0 && j == 0)cout << UPPER_LEFT_ANGLE;
+			else if (i == 0 && j == n)cout << UPPER_RIGHT_ANGLE;
+			else if (i == n && j == 0)cout << DOWN_LEFT_ANGLE;
+			else if (i == n && j == n)cout << DOWN_RIGHT_ANGLE;
+			else if (i == 0 || i == n)cout << HORIZ_LINE;
+			else if (j == 0 || j == n)cout << VERT_LINE;
+			else
+			{
+				/*if ((i + j) % 2 == 0)cout << WHITE_BOX;
+				else cout << "  ";*/
+				cout << ((i + j) % 2 == 0 ? WHITE_BOX : BLACK_BOX);
+			}
+		}
 		cout << endl;
 	}
 }
--- ControlStructures/HomeWorkNested/main.cpp
+++ ControlStructures/HomeWorkNested/main.cpp
@@ -1,25 +1,28 @@
 #include <iostream>
+#include<conio.h>
 using namespace std;
-using std::cout;
-using std::cin;
-using std::endl;
 
-#define SQUARE
-#define TRIANGLE_1
-#define TRIANGLE_2
-#define TRIANGLE_2_2
-#define TRIANGLE_3
-#define TRIANGLE_4
-#define ROMB
-#define PLUSIKI_MINUSIKI
+//#define SQUARE
+//#define TRIANGLE_1
+//#define TRIANGLE_2
+//#define TRIANGLE_3
+//#define TRIANGLE_4
+#define RUM
+//#define PLUS_MINUS
+//#define CHESS_BOARD
+//#define CHESS_BOARD_2
+#define CHESS_BOARD_3
+
+
 
 void main()
-{
-	setlocale(LC_ALL, "ru");
-#ifdef SQUARE
-	int n;
-	cout << "¬ведите количество итераций: "; cin >> n;
 
+{
+	setlocale(LC_ALL, "Russian");
+
+	int n;
+	cout << "¬ведите длину стороны фигуры: "; cin >> n;
+#ifdef SQUARE //выводим квадрат
 	for (int i = 0; i < n; i++)
 	{
 		for (int j = 0; j < n; j++)
@@ -27,168 +30,177 @@ void main()
 			cout << "* ";
 		}
 		cout << endl;
+
 	}
+	cout << endl;
 #endif // SQUARE
 
-
-
-
-#ifdef TRIANGLE_1
-	int k;
-	cout << "¬ведите количество итераций: "; cin >> k;
-
-	for (int i = 0; i < k; i++)
+#ifdef TRIANGLE_1 //выводим пр€моугольный треугольник
+	for (int i = 0; i < n; i++)
 	{
-		for (int j = 0; j < i + 1; j++)
+		for (int j = 0; j <= i; j++)
 		{
 			cout << "* ";
-		}
-		cout << '\n';
-	}
 
-#endif // TRIANGLE_1
-
-
-
-#ifdef TRIANGLE_2
-	int z;
-	cout << "¬ведите количество итераций: "; cin >> z;
-
-	for (int i = 0; i < z; i++)
-	{
-		for (int j = 0; j < z - i; j++)
-		{
-
-			cout << "* ";
-		}
-		cout << '\n';
-	}
-#endif // TRIANGLE_2
-
-#ifdef TRIANGLE_2_2
-	int q;
-	cout << "¬ведите количество итераций: "; cin >> q;
-	for (int i = 0; i < q; i++)
-	{
-		for (int j = i; j < q; j++)
-		{
-			cout << "* ";
-		} cout << endl;
-	}
-#endif // TRIANGLE_2_2
-
-
-
-#ifdef TRIANGLE_3
-	int a;
-	cout << "¬ведите количество итераций: "; cin >> a;
-
-	for (int i = 0; i < a; i++) // вывод строк
-	{
-		for (int j = 0; j < i; j++) // вывод пробелов
-		{
-			cout << "  ";
-		}
-		for (int k = i; k < a; k++) // вывод звездочек
-		{
-			cout << "* ";
-		}
-
-
-		cout << '\n';
-	}
-#endif // TRIANGLE_3
-
-
-#ifdef TRIANGLE_4
-	int v;
-	cout << "¬ведите количество итераций: "; cin >> v;
-
-	for (int i = 0; i < v; i++) // вывод строк
-	{
-		for (int j = i; j < a - 1; j++) // вывод пробелов
-		{
-			cout << "  ";
-		}
-		for (int k = 0; k < i + 1; k++) // вывод звездочек
-		{
-			cout << "* ";
-		}
-		cout << '\n';
-	}
-
-#endif // TRIANGLE_4
-
-
-	//int c;
-	//
-	//cout << "¬ведите количесвто итераций: "; cin >> c;
-	//
-	//for (int i = 0; i < c; i++)
-	//{
-	//	for (int j = 0; j < c; j++)
-	//	{
-	//		cout << "* ";
-	//	} cout << endl;
-	//}
-
-
-#ifdef ROMB
-	int l;
-	cout << "¬ведите количество итераций: "; cin >> l;
-
-	for (int i = 0; i < l; i++) // вывод строк
-	{
-		for (int j = i; j < l; j++) // вывод пробелов
-		{
-			cout << " ";
-		}
-		for (int k = 0; k < i; k++) // вывод звездочек
-		{
-			cout << "* ";
 		}
 		cout << endl;
 	}
-	for (int i = 0; i < l; i++) // вывод строк
+	cout << endl;
+#endif // TRIANGLE_1
+
+#ifdef TRIANGLE_2 //выводим перевернутый пр€моугольный треугольник - способ 2
+	for (int i = 0; i < n; i++)
 	{
-		for (int j = 0; j < i; j++) // вывод пробелов
-		{
-			cout << " ";
-		}
-		for (int k = i; k < l; k++) // вывод звездочек
+		for (int j = i; j < n; j++)
 		{
 			cout << "* ";
+
+		}
+		cout << endl;
+	}
+	cout << endl;
+#endif // TRIANGLE_2
+
+#ifdef TRIANGLE_3 //выводим отражение пр€моугольного треугольника через квадрат
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			/*if (j >=i) cout << "* ";
+			else cout << "  ";*/
+			cout << (j >= i ? "* " : "  ");
+		}
+		cout << endl;
+	}
+	cout << endl;
+#endif // TRIANGLE_3
+
+#ifdef TRIANGLE_4	//выводим перевернутое отражение пр€моугольного треугольника через квадрат
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			cout << (j >= n - i - 1 ? "* " : "  ");
+		}
+		cout << endl;
+	}
+	cout << endl;
+#endif // TRIAGLE_4
+
+#ifdef RUM //если считаем, что задаем половину диагонали ромба, принцип построени€ из 2 треугольников
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = i; j < n; j++) cout << " ";
+		cout << "/";
+		for (int j = 0; j < i * 2; j++)	cout << " ";
+		cout << "\\";
+
+		cout << endl;
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j <= i; j++) cout << " ";
+		cout << "\\";
+		for (int j = i; j < n * 2 - i - 2; j++) cout << " ";
+		cout << "/";
+		cout << endl;
+
+	}
+	cout << endl;
+
+#endif // RUM
+
+#ifdef PLUS_MINUS //дл€ четных строк + выводитс€ в четных столбцах, дл€ нечетных + выводитс€ в нечетных
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			cout << ((j - i) % 2 == 0 ? "+ " : "- ");
+
+		}
+		cout << endl;
+	}
+	cout << endl;
+
+#endif // PLUS_MINUS
+
+
+#ifdef CHESS_BOARD
+	for (int i = 0; i < n; i++)
+	{
+		for (int k = 0; k < 2; k++)
+		{
+			for (int j = 0; j < n; j++)
+			{
+				for (int l = 0; l < 2; l++)
+				{
+					if ((j - i) % 2 == 0) cout << "+ ";
+					else cout << "  ";
+				}
+
+			}
+			cout << endl;
 		}
 
-		cout << '\n';
+
 	}
 
-#endif // ROMB
+	cout << endl;
+#endif // CHESS_BOARD
 
-
-
-#ifdef PLUSIKI_MINUSIKI
-	int m;
-	cout << "¬ведите количество итераций: "; cin >> m;
-	for (int i = 0; i < m; i++)
+#ifdef CHESS_BOARD_2
+	setlocale(LC_ALL, "C");
+	for (int i = 0; i < n + 1; i++)
+		cout << char(196) << char(196);
+	cout << endl;
+	for (int i = 0; i < n; i++)
 	{
-		for (int j = 0; j < m; j++)
+		for (int j = 0; j < n + 1; j++)
 		{
-			if (j % 2)
-				cout << "- ";
+			if (j == 0) cout << "|";
+			if (j == n) cout << "|";
 			else
-				cout << "+ ";
-		} cout << endl;
+			{
+				if ((j - i) % 2 == 0) cout << "\x1b[37;47m  \x1b[0m";
+				else
+					cout << "  ";
+			}
+		}
+		cout << endl;
 	}
-#endif // PLUSIKI_MINUSIKI
+	for (int i = n + 1; i <= 2 * n + 1; i++)
+		cout << char(196) << char(196);
+	cout << endl;
 
+#endif // CHESS_BOARD_2
 
+#ifdef CHESS_BOARD_3
+	setlocale(LC_ALL, "C");
 
+	for (int i = 0; i < n + 2; i++)
+	{
+		for (int j = 0; j < n + 2; j++)
 
+		{
+			/*if(i == 0) cout << char(196) << char(196);
+			if (j == 0) cout << "| ";
+			if (j == n + 1) cout << " |";
+			if (i == n+1) cout << char(196) << char(196);*/ //вывести отдельно части рамки не получаетс€
 
+			if (i == 0 || i == n + 1 || j == 0 || j == n + 1) cout << "**";
+			else
+			{
+				if ((j - i) % 2 == 0) cout << "\x1b[37;47m  \x1b[0m";
+				else
+					cout << "  ";
+			}
+		}
+		cout << endl;
+	}
 
+	cout << endl;
 
-
-
+#endif // CHESS_BOARD_3
 }
-
