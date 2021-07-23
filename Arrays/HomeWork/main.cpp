3585127c2461fb532aae02d2f4f4daf208a18d6b
Clayman <Clayman@DESKTOP-DM8E77U>
2021-07-23 15:48:13 +0300

    Sort 2D

--- Arrays/HomeWork/main.cpp
+++ Arrays/HomeWork/main.cpp
@@ -1,91 +1,45 @@
-#include<iostream>
-#include<ctime>
+#include <iostream>
 using namespace std;
-
-using std::cout;
-using std::cin;
-using std::endl;
-using std::srand;
-
-
-//#define RANDARRAY	//a two-dimensional array with random values
-//#define ARRAYSHOWMANY	//a one-dimensional array from 0 to 10 with the output of the number of repetitions for repeating numbers
-#define ARRAYSHOWMANY2 
-//#define ARRAYSHOWMANY3	//memset
+#define tab "\t"
 
 void main()
 {
-	setlocale(LC_ALL, "rus");
-
-#ifdef RANDARRAY
-
-	const int n = 5;	//line of arrow
-	const int k = 2;	//number of line
-
-	int arr[k][n];
-	for (int i = 0; i < n; i++)
+	setlocale(LC_ALL, "Rus");
+	const int ROWS = 8;
+	const int COLS = 5;
+	const int SIZE = ROWS * COLS;
+	int arr[ROWS][COLS] = {};
+	for (int i = 0; i < ROWS; i++)
 	{
-		arr[0][i] = rand();
-		arr[1][i] = arr[0][i] % 100;
-	}
-	for (int i = 0; i < n; i++)
-	{
-		cout << arr[0][i] << "-" << arr[1][i] << "  ";
-	} cout << endl;
-#endif // RANDARRAY
-
-#ifdef ARRAYSHOWMANY
-	//srand(time(0));	//un-comment to check different options (variants)
-
-	const int S = 10;
-	int arr[S] = {};
-	int n = 1;
-	for (int i = 0; i < S; i++)	//filling the array with random numbers
-	{
-		arr[i] = rand() % 10;
-	}
-	for (int i = 0; i < S; i++)	//sortirovochka
-		for (int j = i + 1; j < S; j++)
-			if (arr[i] > arr[j]) swap(arr[i], arr[j]);
-	for (int i = 0; i < S; cout << arr[i] << "\t", i++) {}	//just for visual, what numbers are in the array
-	cout << endl;
-	for (int i = 0; i < S; i++)
-	{
-		if (arr[i] == arr[i + 1])
+		for (int j = 0; j < COLS; j++)
 		{
-			cout << "The number - " << arr[i];
-			while (arr[i] == arr[i + 1]) n += 1, i++;
-			cout << " is repeated " << n << " times" << endl; n = 1;
+			arr[i][j] = rand() % SIZE;
 		}
 	}
-#endif // ARRAYSHOWMANY
-
-#ifdef ARRAYSHOWMANY2
-	const int S = 10, r = 2;	//change S if you want
-	int arr[S] = {};
-	int arr2[r][S] = { {},{} };
-	int n = 0;
-	//srand(time(0));
-	for (int i = 0; i < S; cout << arr[i++] << " ")
+	for (int i = 0; i < ROWS; i++)
 	{
-		arr[i] = rand() % 10+100;	// change if you want
-		for (int j = 0; j < i; j++)
+		for (int j = 0; j < COLS; j++) cout << arr[i][j] << "\t"; cout << endl << endl;
+	}
+	double sum = 0;
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLS; j++)
 		{
-			if (arr[i] == arr[j] && i > arr2[1][arr[i]])
-			{
-				arr2[0][arr[i]]++;
-				arr2[1][arr[i]] = i;
-			}
+			sum += arr[i][j];
 		}
 	}
-	cout << endl;
-	for (int i = 0; i < S; i++)
-		if (arr2[0][i] > 1)
-			cout << "Number " << i << " was appeared " << arr2[0][i] + 1 << " times" << endl;
-		else if (arr2[0][i] > 0)
-			cout << "Number " << i << " was repeated " << arr2[0][i] + 1 << " times" << endl;
-
-#endif // ARRAYSHOWMANY2
-
-
+	cout << "Сумма значений элементов массива = " << sum << endl;
+	cout << "Среднее арифметическое значений элементов массива = " << sum / SIZE << endl;
+	int max = arr[0][0];
+	int min = arr[0][0];
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			if (arr[i][j] > max)max = arr[i][j];
+			if (arr[i][j] < min)min = arr[i][j];
+		}
+	}
+	cout << "Максимальное значение = " << max << endl;
+	cout << "Минимальное значение = " << min << endl;
 }
