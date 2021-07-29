9faffaa046c7d12c94230fdb1b0be53a29a5cb2b
Clayman <Clayman@DESKTOP-DM8E77U>
2021-07-29 22:24:25 +0300

    Function overloads

--- Functions/Arrays/main.cpp
+++ Functions/Arrays/main.cpp
@@ -1,29 +1,59 @@
 #include<iostream>
 using namespace std;
 
+#define delimiter "\n--------------------------------------\n"
+
 void FillRand(int arr[], const int n);
+void FillRand(double arr[], const int n);
+
 void Print(int arr[], const int n);
+void Print(double arr[], const int n);
+
 void Sort(int arr[], const int n);
+void Sort(double arr[], const int n);
+
+int  Sum(int arr[], const int n);
+double Sum(double arr[], const int n);
+
+double Avg(int arr[], const int n);
+double Avg(double arr[], const int n);
+
+int  minValueIn(int arr[], const int n);
+int  maxValueIn(int arr[], const int n);
+
+double minValueIn(double arr[], const int n);
+double maxValueIn(double arr[], const int n);
 
 void main()
 {
 	setlocale(LC_ALL, "");
-
+	cout << "Int:" << endl;
 	const int n = 5;
 	int arr[n];
 	FillRand(arr, n);
 	Print(arr, n);
 	Sort(arr, n);
 	Print(arr, n);	
+	cout << "Сумма элементов массива: " << Sum(arr, n) << endl;
+	cout << "Среднее арифметическое элементов массива: " << Avg(arr, n) << endl;
+	cout << "Минимальное значение в массиве: " << minValueIn(arr, n) << endl;
+	cout << "Максимальное значение в массиве: " << maxValueIn(arr, n) << endl;
+
+	cout << delimiter << endl;
 
 	/////////////////////////////////////////////////////////////
 
+	cout << "Double:" << endl;
 	const int B_SIZE = 8;
-	int brr[B_SIZE];
+	double brr[B_SIZE];
 	FillRand(brr, B_SIZE);
 	Print(brr, B_SIZE);
-	Sort(brr, B_SIZE);
+	//Sort(brr, B_SIZE);
 	Print(brr, B_SIZE);
+	/*cout << "Сумма элементов массива: " << Sum(brr, B_SIZE) << endl;
+	cout << "Среднее арифметическое элементов массива: " << Avg(brr, B_SIZE) << endl;
+	cout << "Минимальное значение в массиве: " << minValueIn(brr, B_SIZE) << endl;
+	cout << "Максимальное значение в массиве: " << maxValueIn(brr, B_SIZE) << endl;*/
 }
 
 void FillRand(int arr[], const int n)
@@ -34,6 +64,15 @@ void FillRand(int arr[], const int n)
 		arr[i] = rand() % 100;
 	}
 }
+void FillRand(double arr[], const int n)
+{
+	//Заполнение массива случайными числами:
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = double(rand() % 10000)/100;
+	}
+}
+
 void Print(int arr[], const int n)
 {
 	//Вывод массива на экран:
@@ -43,6 +82,16 @@ void Print(int arr[], const int n)
 	}
 	cout << endl;
 }
+void Print(double arr[], const int n)
+{
+	//Вывод массива на экран:
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << "\t";
+	}
+	cout << endl;
+}
+
 void Sort(int arr[], const int n)
 {
 	//Сортировка:
@@ -58,4 +107,36 @@ void Sort(int arr[], const int n)
 			}
 		}
 	}
+}
+
+int  Sum(int arr[], const int n)
+{
+	int Sum = 0;
+	for (int i = 0; i < n; i++)
+	{
+		Sum += arr[i];
+	}
+	return Sum;
+}
+double Avg(int arr[], const int n)
+{
+	return (double)Sum(arr, n) / n;
+}
+int  minValueIn(int arr[], const int n)
+{
+	int min = arr[0];
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] < min)min = arr[i];
+	}
+	return min;
+}
+int  maxValueIn(int arr[], const int n)
+{
+	int max = arr[0];
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] > max)max = arr[i];
+	}
+	return max;
 }
