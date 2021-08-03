34573c865f2aeb3187b87ebfe0ffbddfba4a9fb0
Clayman <Clayman@DESKTOP-DM8E77U>
2021-08-03 21:31:17 +0300

    Separation DONE.

--- Functions/Arrays/main.cpp
+++ Functions/Arrays/main.cpp
@@ -1,42 +1,15 @@
-#include<iostream>
-using namespace std;
-using std::cin;
-using std::cout;
-using std::endl;
+#include"stdafx.h"
+#include"FillRand.h"
+#include"Print.h"
+#include"Sort.h"
+#include"Statistics.h"
 
-#define delimiter "\n--------------------------------------\n"
-
-const int ROWS = 3;
-const int COLS = 8;
-
-void FillRand(int arr[], const int n);
-void FillRand(double arr[], const int n);
-void FillRand(int arr[ROWS][COLS], const int ROWS, const int COLS);
-
-void Print(int arr[], const int n);
-void Print(double arr[], const int n);
-void Print(int arr[ROWS][COLS], const int ROWS, const int COLS);
-
-void Sort(int arr[], const int n);
-void Sort(double arr[], const int n);
-
-int  Sum(int arr[], const int n);
-double Sum(double arr[], const int n);
-
-double Avg(int arr[], const int n);
-double Avg(double arr[], const int n);
-
-int  minValueIn(int arr[], const int n);
-int  maxValueIn(int arr[], const int n);
-
-double minValueIn(double arr[], const int n);
-double maxValueIn(double arr[], const int n);
-
-//#define HOME_WORK
+#define HOME_WORK
 
 void main()
 {
 	setlocale(LC_ALL, "");
+
 #ifdef HOME_WORK
 	cout << "Int:" << endl;
 	const int n = 5;
@@ -75,142 +48,5 @@ void main()
 	Print(i_arr_2, ROWS, COLS);
 }
 
-void FillRand(int arr[], const int n)
-{
-	//Заполнение массива случайными числами:
-	for (int i = 0; i < n; i++)
-	{
-		arr[i] = rand() % 100;
-	}
-}
-void FillRand(double arr[], const int n)
-{
-	//Заполнение массива случайными числами:
-	for (int i = 0; i < n; i++)
-	{
-		arr[i] = double(rand() % 10000)/100;
-	}
-}
-void FillRand(int arr[ROWS][COLS], const int ROWS, const int COLS)
-{
-	for (int i = 0; i < ROWS; i++)
-	{
-		for (int j = 0; j < COLS; j++)
-		{
-			arr[i][j] = rand() % 100;
-		}
-	}
-}
 
-void Print(int arr[], const int n)
-{
-	//Вывод массива на экран:
-	for (int i = 0; i < n; i++)
-	{
-		cout << arr[i] << "\t";
-	}
-	cout << endl;
-}
-void Print(double arr[], const int n)
-{
-	//Вывод массива на экран:
-	for (int i = 0; i < n; i++)
-	{
-		cout << arr[i] << "\t";
-	}
-	cout << endl;
-}
-void Print(int arr[ROWS][COLS], const int ROWS, const int COLS)
-{
-	for (int i = 0; i < ROWS; i++)
-	{
-		for (int j = 0; j < COLS; j++)
-		{
-			cout << arr[i][j] << "\t";
-		}
-		cout << endl;
-	}
-}
 
-void Sort(int arr[], const int n)
-{
-	//Сортировка:
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = i + 1; j < n; j++)
-		{
-			if (arr[j] < arr[i])
-			{
-				int buffer = arr[i];
-				arr[i] = arr[j];
-				arr[j] = buffer;
-			}
-		}
-	}
-}
-
-int  Sum(int arr[], const int n)
-{
-	int Sum = 0;
-	for (int i = 0; i < n; i++)
-	{
-		Sum += arr[i];
-	}
-	return Sum;
-}
-double Sum(double arr[], const int n)
-{
-	double Sum = 0;
-	for (int i = 0; i < n; i++)
-	{
-		Sum += arr[i];
-	}
-	return Sum;
-}
-
-double Avg(int arr[], const int n)
-{
-	return (double)Sum(arr, n) / n;
-}
-double Avg(double arr[], const int n)
-{
-	return (double)Sum(arr, n) / n;
-}
-
-int  minValueIn(int arr[], const int n)
-{
-	int min = arr[0];
-	for (int i = 0; i < n; i++)
-	{
-		if (arr[i] < min)min = arr[i];
-	}
-	return min;
-}
-double minValueIn(double arr[], const int n)
-{
-	double min = arr[0];
-	for (int i = 0; i < n; i++)
-	{
-		if (arr[i] < min)min = arr[i];
-	}
-	return min;
-}
-
-int  maxValueIn(int arr[], const int n)
-{
-	int max = arr[0];
-	for (int i = 0; i < n; i++)
-	{
-		if (arr[i] > max)max = arr[i];
-	}
-	return max;
-}
-double maxValueIn(double arr[], const int n)
-{
-	double max = arr[0];
-	for (int i = 0; i < n; i++)
-	{
-		if (arr[i] > max)max = arr[i];
-	}
-	return max;
-}
