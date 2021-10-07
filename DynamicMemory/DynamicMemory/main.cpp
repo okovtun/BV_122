dcd14727b60cdcb2ca194b2a0f0d4f24510de889
Clayman <Clayman@DESKTOP-DM8E77U>
2021-10-07 18:45:51 +0300

    Templation DONE.

--- DynamicMemory/DynamicMemory/main.cpp
+++ DynamicMemory/DynamicMemory/main.cpp
@@ -1,25 +1,32 @@
-﻿#include<iostream>
-using std::cin;
+﻿#include<iostream>	//#... Директива языка C
+using std::cin;		//using - Директива языка C++
 using std::cout;
 using std::endl;
 
-int** allocate(const unsigned int rows, const unsigned int cols);
-void  clear(int** arr, const unsigned int rows);
+template<typename T>T** allocate(const unsigned int rows, const unsigned int cols);
+template<typename T>void  clear(T** arr, const unsigned int rows);
 
 void FillRand(int arr[], const unsigned int n, int minRand = 0, int maxValue = 100);
+void FillRand(double arr[], const unsigned int n, int minRand = 0, int maxValue = 100);
+void FillRand(char arr[], const unsigned int n);
 void FillRand(int** arr, const unsigned int rows, const unsigned int cols, int minRand = 0, int maxRand = 100);
-void Print(int arr[], const unsigned int n);
-void Print(int** arr, const unsigned int rows, const unsigned int cols);
+void FillRand(double** arr, const unsigned int rows, const unsigned int cols, int minRand = 0, int maxRand = 100);
+void FillRand(char** arr, const unsigned int rows, const unsigned int cols);
+
+template<typename T>void Print(T arr[], const unsigned int n);
+template<typename T>void Print(T** arr, const unsigned int rows, const unsigned int cols);
 //CamelCaseStyle: BigCamel, smallCamel
 //snake_case_style
-int* push_back(int arr[], int& n, int value);
-int* pop_back(int arr[], int& n);
+template<typename T>int* push_back(T arr[], int& n, T value);
+template<typename T>int* pop_back(T arr[], int& n);
 
-int** push_row_back(int** arr, unsigned int& rows, const unsigned int cols);
-void  push_col_back(int** arr, const unsigned int rows, unsigned int& cols);
+template<typename T>T** push_row_back(T** arr, unsigned int& rows, const unsigned int cols);
+template<typename T>void  push_col_back(T** arr, const unsigned int rows, unsigned int& cols);
 
 //#define DYNAMIC_MEMORY_1
 #define DYNAMIC_MEMORY_2
+
+#define DATA_TYPE double
 
 void main()
 {
@@ -43,11 +50,33 @@ void main()
 	delete[] arr;
 #endif // DYNAMIC_MEMORY_1
 
+	int user_type;
+	cout << "Выберите тип данных:\n";
+	cout << "i - int\n";
+	cout << "d - double\n";
+	cout << "c - char\n";
+	cin >> user_type;
+	/*typedef int DataType;
+	switch (user_type)
+	{
+	case 'i': typedef int DataType; break;
+	case 'd': typedef double DataType; break;
+	case 'c': typedef char DataType; break;
+	}*/
+	//using DataType = int;
+	switch (user_type)
+	{
+	case 'i': using DataType = int; break;
+	case 'd': using DataType = double; break;
+	case 'c': using DataType = char; break;
+	}
+
 	unsigned int rows;
 	unsigned int cols;
 	cout << "Введите количество строк: "; cin >> rows;
 	cout << "Введите количество элементов строки: "; cin >> cols;
-	int** arr = allocate(rows, cols);
+	//typedef double DataType;
+	DataType** arr = allocate<DataType>(rows, cols);
 
 	//////////////////////////////////////////////////////////////////////////////
 	//////		ОБРАЩЕНИЕ К ЭЛЕМЕНТАМ ДВУМЕРНОГО ДИНАМИЧЕСКОГО МАССИВА:		//////
@@ -57,7 +86,7 @@ void main()
 	cout << "\n------------------------------------------------------------\n";
 
 	arr = push_row_back(arr, rows, cols);
-	FillRand(arr[rows - 1], cols, 200, 300);
+	//FillRand(arr[rows - 1], cols, 200, 300);
 
 	for (int i = 0; i < rows; i++)
 	{
@@ -68,28 +97,30 @@ void main()
 		cout << endl;
 	}
 
+	cout << "\n------------------------------------------------------------\n";
 	push_col_back(arr, rows, cols);
 	for (int i = 0; i < rows; i++)arr[i][cols - 1] = rand();
 	Print(arr, rows, cols);
+	cout << "\n------------------------------------------------------------\n";
 
 	clear(arr, rows);
 }
 
-int** allocate(const unsigned int rows, const unsigned int cols)
+template<typename T>T** allocate(const unsigned int rows, const unsigned int cols)
 {
 	///////////////////////////////////////////////////////////////////////////////
 	/////////////	ОБЪЯВЛЕНИЕ ДВУМЕРНОГО ДИНАМИЧЕСКОГО МАССИВА:	///////////////
 	///////////////////////////////////////////////////////////////////////////////
 	//1) Объявляем указатель на указатель, и сохраняем в него адрес массива указателей:
-	int** arr = new int*[rows];
+	T** arr = new T*[rows];
 	//2) Создаем строки двумерного динамического массива:
 	for (int i = 0; i < rows; i++)
 	{
-		arr[i] = new int[cols] {};
+		arr[i] = new T[cols]{};
 	}
 	return arr;
 }
-void  clear(int** arr, const unsigned int rows)
+template<typename T>void  clear(T** arr, const unsigned int rows)
 {
 	///////////////////////////////////////////////////////////////////////////////
 	/////////////	 УДАЛЕНИЕ ДВУМЕРНОГО ДИНАМИЧЕСКОГО МАССИВА:		///////////////
@@ -110,6 +141,24 @@ void FillRand(int arr[], const unsigned int n, int minRand, int maxRand)
 		arr[i] = rand() % (maxRand - minRand) + minRand;
 	}
 }
+void FillRand(double arr[], const unsigned int n, int minRand, int maxRand)
+{
+	minRand *= 100;
+	maxRand *= 100;
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = rand() % (maxRand - minRand) + minRand;
+		arr[i] /= 100;
+	}
+}
+void FillRand(char arr[], const unsigned int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = rand();
+	}
+}
+
 void FillRand(int** arr, const unsigned int rows, const unsigned int cols, int minRand, int maxRand)
 {
 	for (int i = 0; i < rows; i++)
@@ -120,7 +169,31 @@ void FillRand(int** arr, const unsigned int rows, const unsigned int cols, int m
 		}
 	}
 }
-void Print(int arr[], const unsigned int n)
+void FillRand(double** arr, const unsigned int rows, const unsigned int cols, int minRand, int maxRand)
+{
+	minRand *= 100;
+	maxRand *= 100;
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < cols; j++)
+		{
+			arr[i][j] = rand() % (maxRand - minRand) + minRand;
+			arr[i][j] /= 100;
+		}
+	}
+}
+void FillRand(char** arr, const unsigned int rows, const unsigned int cols)
+{
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < cols; j++)
+		{
+			arr[i][j] = rand();
+		}
+	}
+}
+
+template<typename T>void Print(T arr[], const unsigned int n)
 {
 	for (int i = 0; i < n; i++)
 	{
@@ -128,7 +201,7 @@ void Print(int arr[], const unsigned int n)
 	}
 	cout << endl;
 }
-void Print(int** arr, const unsigned int rows, const unsigned int cols)
+template<typename T>void Print(T** arr, const unsigned int rows, const unsigned int cols)
 {
 	for (int i = 0; i < rows; i++)
 	{
@@ -140,7 +213,7 @@ void Print(int** arr, const unsigned int rows, const unsigned int cols)
 	}
 }
 
-int* push_back(int arr[], int& n, int value)
+template<typename T>T* push_back(T arr[], int& n, T value)
 {
 	//////////////////////////////////////////////////////////////////
 	///////////////		Добавление элемента в массив	//////////////
@@ -166,7 +239,7 @@ int* push_back(int arr[], int& n, int value)
 	return arr;
 }
 
-int* pop_back(int arr[], int& n)
+template<typename T>T* pop_back(T arr[], int& n)
 {
 	int* buffer = new int[--n];
 	for (int i = 0; i < n; i++)buffer[i] = arr[i];
@@ -174,10 +247,10 @@ int* pop_back(int arr[], int& n)
 	return buffer;
 }
 
-int** push_row_back(int** arr, unsigned int& rows, const unsigned int cols)
+template<typename T>T** push_row_back(T** arr, unsigned int& rows, const unsigned int cols)
 {
 	//1) Создаем буферный массив указателей:
-	int** buffer = new int*[rows + 1]{};
+	T** buffer = new T*[rows + 1]{};
 	//2) Копируем адреса строк в буферный массив указателей:
 	for (int i = 0; i < rows; i++)
 	{
@@ -188,18 +261,18 @@ int** push_row_back(int** arr, unsigned int& rows, const unsigned int cols)
 	//4) Подменяем исходный массив буферным:
 	arr = buffer;
 	//5) Добавляем в массив новую строку:
-	arr[rows] = new int[cols] {};
+	arr[rows] = new T[cols]{};
 	//6) Увеличиваем количество строк:
 	rows++;
 	//7) Возвращаем новый массив на место вызова:
 	return arr;
 }
-void  push_col_back(int** arr, const unsigned int rows, unsigned int& cols)
+template<typename T>void  push_col_back(T** arr, const unsigned int rows, unsigned int& cols)
 {
 	for (int i = 0; i < rows; i++)
 	{
 		//1) Создаем буферную строку, размером на 1 элемент больше:
-		int* buffer = new int[cols + 1]{};
+		T* buffer = new T[cols + 1]{};
 		//2) Копируем исходную строку в буферную:
 		for (int j = 0; j < cols; j++)
 		{
